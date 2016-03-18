db2d8364929a07fe53f1213407571562542528fd
Arthur Pasztor <turi_pasztor@yahoo.com>
2016-03-18 12:45:44 +0200

    Add tasks manually

--- app/src/main/java/com/example/arthur/goodhabits/MyHabitActivity.java
+++ app/src/main/java/com/example/arthur/goodhabits/MyHabitActivity.java
@@ -1,12 +1,21 @@
 package com.example.arthur.goodhabits;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.text.Editable;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MyHabitActivity extends AppCompatActivity {
+
+    String mName = "";
+
+    TextView mPrintView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,31 @@ public class MyHabitActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        mPrintView = (TextView) findViewById(R.id.myhabit_habit_value);
+
+        findViewById(R.id.myhabit_save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nameField = (EditText) findViewById(R.id.myhabit_healthy_habit_input);
+                Editable nameEditable = nameField.getText();
+                mName += nameEditable.toString()+'\n';
+                mPrintView.setText(mName);
+                nameField.setText("");
+            }
+        });
+
+        findViewById(R.id.myhabit_finish_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nameField = (EditText) findViewById(R.id.myhabit_healthy_habit_input);
+                Editable nameEditable = nameField.getText();
+                mName += nameEditable.toString()+'\n';
+                Intent intent = new Intent(MyHabitActivity.this, Sec.class);
+                intent.putExtra("data", mName);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -29,4 +63,5 @@ public class MyHabitActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 }
